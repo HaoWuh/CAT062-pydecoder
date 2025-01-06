36825b1ba56046df75680887cf405640f4cad08b
Hao Wu <haowuh@login23.chn.perlmutter.nersc.gov>
2025-01-06 02:41:08 -0800

    updates

--- .ipynb_checkpoints/real_decode_test-checkpoint.py
+++ .ipynb_checkpoints/real_decode_test-checkpoint.py
@@ -8,10 +8,28 @@ if __name__ == '__main__':
     path= rf"data/binary.raw"
     RR= Raw_Reader(path)
     data_list= RR.preprocessed_data
+    
+    failed_data= []
         
     results= []
     for i, d in tqdm(enumerate(data_list)):
-        BD= byte_decoder(data= d)
-        results.append(BD.process_byte_data())
+        try:
+            BD= byte_decoder(data= d)
+            results.append(BD.process_byte_data())
+        except:
+            print("data ", i, " failed")
+            print("Info:")
+            print(d)
+            print("len: ", len(d))
+            failed_data.append(d)
 
     byte_decoder.save2json_static(results)
+    
+    print("Saved!")
+    print("######################")
+    
+    print("Failed data:")
+    for i, fd in enumerate(failed_data):
+        print("No. ", i)
+        print("length: ", len(fd))
+        print(fd)
